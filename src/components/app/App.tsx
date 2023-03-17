c406c291349bb06d7c81402ce2c280e52f0a977f
Anton Vyun <zeta664433@gmail.com>
2023-03-17 15:44:30 +0300

    feat: add route

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import AboutPage from '../aboutPage/aboutPage';
+import MainPage from '../mainPage/MainPage';
+import ErrorPage from '../errorPage/ErrorPage';
 import './app.scss';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
