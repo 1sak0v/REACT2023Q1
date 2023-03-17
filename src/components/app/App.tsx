e90fd0df9d9d997afa18ec85cf974b680f696749
Anton Vyun <zeta664433@gmail.com>
2023-03-17 20:50:51 +0300

    feat: add redirect to 404

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import AboutPage from '../aboutPage/aboutPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Header from '../header/Header';
 import MainPage from '../mainPage/MainPage';
+import AboutPage from '../aboutPage/aboutPage';
 import ErrorPage from '../errorPage/ErrorPage';
+
 import './app.scss';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="about" element={<AboutPage />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="404" element={<ErrorPage />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
     </div>
   );
