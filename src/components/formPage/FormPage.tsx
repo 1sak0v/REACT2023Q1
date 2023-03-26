3bd8b232f4c0ae94b0dcc40bae9cf985849b9b15
Anton Vyun <zeta664433@gmail.com>
2023-03-26 20:48:55 +0300

    feat: add profile list and state

--- src/components/formPage/FormPage.tsx
+++ src/components/formPage/FormPage.tsx
@@ -1,16 +1,51 @@
+import { Component } from 'react';
+import ProfileList from '../../profileList/ProfileList';
+import { TProfiles } from '../../types/types';
+
 import Form from '../form/Form';
 
-const FormPage = () => {
-  return (
-    <main className="main">
-      <div className="container">
-        <div className="main__wrapper">
-          <h2 className="main__title">Form</h2>
-          <Form />
+class FormPage extends Component<never, TProfiles> {
+  id = 1;
+  state = {
+    profiles: [],
+  };
+
+  addProfile = (
+    name: string,
+    birthday: string,
+    continent: string,
+    skills: string[],
+    gender: string,
+    image: string
+  ): void => {
+    const newProfile = {
+      id: this.id++,
+      name,
+      birthday,
+      continent,
+      skills,
+      gender,
+      image,
+    };
+    this.setState(({ profiles }) => ({
+      profiles: [...profiles, newProfile],
+    }));
+  };
+
+  render() {
+    const { profiles } = this.state;
+    return (
+      <main className="main">
+        <div className="container">
+          <div className="main__wrapper">
+            <h2 className="main__title">Form</h2>
+            <Form addProfile={this.addProfile} />
+            <ProfileList profiles={profiles} />
+          </div>
         </div>
-      </div>
-    </main>
-  );
-};
+      </main>
+    );
+  }
+}
 
 export default FormPage;
---
+++ src/profileList/ProfileList.tsx
@@ -0,0 +1,15 @@
+import Profile from '../components/profile/Profile';
+import { TProfiles } from '../types/types';
+
+import './profileList.scss';
+
+const ProfileList = (props: TProfiles) => {
+  const profiles = props.profiles.map((prof) => <Profile key={prof.id} {...prof} />);
+  return (
+    <div className="profile">
+      <ul className="profile__list">{profiles}</ul>
+    </div>
+  );
+};
+
+export default ProfileList;
