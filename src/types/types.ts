8025c8f1822671ebf1916b50c4b17618ea8361e5
Anton Vyun <zeta664433@gmail.com>
2023-03-26 20:49:04 +0300

    feat: add types

--- src/types/types.ts
+++ src/types/types.ts
@@ -12,3 +12,39 @@ export type TData = {
   name: string;
   thumbnail: string;
 };
+
+export type TProfile = {
+  id: number;
+  name: string;
+  birthday: string;
+  continent: string;
+  skills: string[];
+  gender: string;
+  image: string;
+};
+
+export type TProfiles = {
+  profiles: TProfile[];
+};
+
+export interface IFormProps {
+  addProfile(
+    name: string,
+    birthday: string,
+    continent: string,
+    skills: string[],
+    gender: string,
+    image: string
+  ): void;
+}
+
+export interface IFormState {
+  firstNameError: boolean;
+  birthdayError: boolean;
+  continentError: boolean;
+  skillsError: boolean;
+  genderError: boolean;
+  fileError: boolean;
+  success: boolean;
+  disabledSubmit: boolean;
+}
