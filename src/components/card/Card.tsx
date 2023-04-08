b4d0b2fe49f99d8e0bcb581d8e118de388d4330a
Anton Vyun <zeta664433@gmail.com>
2023-04-08 14:53:53 +0300

    feat: add request to api for cards

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,12 +2,11 @@ import { TData } from '../../types/types';
 
 import './card.scss';
 
-const Card = ({ name, description, thumbnail }: TData) => {
+const Card = ({ name, thumbnail }: TData) => {
   return (
     <li className="card-item">
       <img src={thumbnail} alt={name} className="card-item__thumbnail" />
       <h2 className="card-item__title">{name}</h2>
-      <div className="card-item__descr">{description}</div>
     </li>
   );
 };
--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -1,13 +1,45 @@
-import data from '../../data/data';
+import { useEffect, useState } from 'react';
+
+import { TCardListProps, TCharacter } from '../../types/types';
+import useMarvelService from '../../service/marvelService';
 import Card from '../card/Card';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './cardList.scss';
 
-const CardList = () => {
-  const cards = data.map((card) => <Card key={card.id} {...card} />);
+const CardList = ({ search }: TCardListProps) => {
+  const [characters, setCharacters] = useState<TCharacter[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  const { getAllCharacters } = useMarvelService();
+
+  useEffect(() => {
+    setLoading(true);
+    getAllCharacters(search)
+      .then((data) => {
+        setCharacters(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
+  }, [search]);
+
+  const cards = characters.map((card) => <Card key={card.id} {...card} />);
+  const spinner = loading ? <Spinner /> : null;
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const content = loading || error ? null : cards;
   return (
     <div className="card">
-      <ul className="card__list">{cards}</ul>
+      {spinner}
+      {errorMessage}
+      <ul className="card__list">{content}</ul>
+      {cards.length === 0 && !loading && !error && (
+        <p className="card__not-found">Characters not found</p>
+      )}
     </div>
   );
 };
