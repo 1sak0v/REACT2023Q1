9acaa3a458422901201526f22bd789180b91eb5b
Anton Vyun <zeta664433@gmail.com>
2023-04-09 18:43:46 +0300

    feat: add modal view by using portal

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,10 +1,10 @@
-import { TData } from '../../types/types';
+import { TCardProps } from '../../types/types';
 
 import './card.scss';
 
-const Card = ({ name, thumbnail }: TData) => {
+const Card = ({ id, name, thumbnail, onOpen }: TCardProps) => {
   return (
-    <li className="card-item">
+    <li className="card-item" onClick={() => onOpen(id)}>
       <img src={thumbnail} alt={name} className="card-item__thumbnail" />
       <h2 className="card-item__title">{name}</h2>
     </li>
