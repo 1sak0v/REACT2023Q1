fb53457d071ba6dd55ae4c026107d24715ee1b9c
Anton Vyun <zeta664433@gmail.com>
2023-04-16 21:19:31 +0300

    feat: add styles to error message

--- src/components/errorMessage/ErrorMessage.tsx
+++ src/components/errorMessage/ErrorMessage.tsx
@@ -2,10 +2,10 @@ import error from './close.png';
 
 const ErrorMessage = () => {
   return (
-    <>
+    <div style={{ margin: '20 auto', textAlign: 'center' }}>
       <p style={{ fontSize: 24 }}>Some error, please repeat your request</p>
       <img style={{ width: 150, marginTop: 20 }} src={error} alt="error" />
-    </>
+    </div>
   );
 };
 
